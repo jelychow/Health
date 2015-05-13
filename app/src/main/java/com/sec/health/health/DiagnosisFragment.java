615791de1524535681b20d39094d51cdce5a8952
zhou <jelychow@gmail.com>
2015-05-13 18:48:33 +0800

    commit by zhou add new part my detail for this

--- app/src/main/java/com/sec/health/health/DiagnosisFragment.java
+++ app/src/main/java/com/sec/health/health/DiagnosisFragment.java
@@ -1,11 +1,15 @@
 package com.sec.health.health;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.sec.health.health.diagosis.MyDiagnosis;
 
 
 /**
@@ -13,7 +17,7 @@ import android.view.ViewGroup;
  */
 public class DiagnosisFragment extends Fragment {
 
-
+    private Button btn_appoint;
     public DiagnosisFragment() {
         // Required empty public constructor
     }
@@ -22,9 +26,20 @@ public class DiagnosisFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diagnosis, container, false);
+        View view = inflater.inflate(R.layout.fragment_diagnosis, container, false);
+        btn_appoint = (Button) view.findViewById(R.id.btn_appoint);
+        return view;
     }
 
-
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        btn_appoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MyDiagnosis.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
